9498d71b2795bcf94db82350c0126fe93e4358b7
Alpha-Phoenix <michael.douglas1995@hotmail.com>
2015-05-29 15:03:55 -0300

    Update quick_sort_helper.h

--- quick_sort_helper.h
+++ quick_sort_helper.h
@@ -1,33 +1,49 @@
-#ifndef QSORTHELPER_H
-#define QSORTHELPER_H
+#ifndef MERGESORTHELPER_H
+#define MERGESORTHELPER_H
 
-int* qsort (int *v, const int beg, const int end) {
-    register int i = beg;
-    register int j = end;
-    register int pivot = v[(i + j) / 2];
+#include <cstdlib>
+#include <memory.h>
 
-    while (i < j) {
-        while (v[i] < pivot && i <  end)
-            i++;
+void merge(int *v, int size) {
 
-        while (v[j] > pivot && j > beg)
-            j--;
+    int middle = size / 2; // 7 8 (1) 2 3    middle = 2
+	                       //      ^
+    int v1Size = middle;   // v1Size = 2
+    int v2Size = size - v1Size; // v2Size = 3
 
-        if (i <= j) {
-            int swap = v[i];
-            v[i] = v[j];
-            v[j] = swap;
-            i++;
-            j--;
-        }
+    int *v2 = v + middle; // 7 8 1 2 3  v2 aponta para 1 = v[2]
+						  //     ^
+
+	//								             		   v v v
+	// Nesse ponto temos "2" vetores  7 8 <- v(original)   1 2 3 <- v2
+	//              				  ^ ^
+
+    int *temp = new int[size];
+    if (temp == nullptr)
+        exit(EXIT_FAILURE);
+
+    register int i = 0, j = 0, k = 0;
+
+    while (i < v1Size && j < v2Size) {
+        if (v[i] <= v2[j])
+            temp[k] = v[i++];
+        else
+            temp[k] = v2[j++];
+        k++;
     }
 
-    if (i < end)
-        qsort(v, i, end);
-    if (j > beg)
-        qsort(v, beg, j);
+    if (i == v1Size) {
+        while (j < v2Size)
+            temp[k++] = v2[j++];
+    } else {
+        while (i < v1Size)
+            temp[k++] = v[i++];
+    }
 
-    return v;
+    for (register int i = 0; i < size; i++)
+        v[i] = temp[i];
+
+    delete [] temp;
 }
 
-#endif // QSORTHELPER_H
+#endif // MERGESORTHELPER_H
